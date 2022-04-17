586264740ed00206ead993f66676c69af0f46e55
KurumiFake <kurumiisshidowife@gmail.com>
2022-04-17 22:51:19 +0800

    skill issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,15 @@ subprojects {
     }
 
     repositories {
+        mavenLocal()
         mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
     }
 }
 
